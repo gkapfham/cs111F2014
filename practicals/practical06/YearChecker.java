5bc3e24a2470dd356bfceba5ed52aff5553a24d1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2014-10-30 08:18:39 -0400

    Adding the new YearChecker file. Seems like a better name for this class.

---
+++ practicals/practical06/YearChecker.java
@@ -0,0 +1,40 @@
+//*****************************
+// CMPSC 111
+// Practical 6
+// October 29, 2014
+//
+// Purpose: determine if user's year input is a leap year,
+// cicada brood II emergence year, or a peak sunspot year.
+//*****************************
+
+import java.util.Scanner;
+
+public class YearChecker 
+{
+    //Create instance variables
+    int year;
+
+    //Create a constructor
+    public YearChecker(int y)
+    {
+        year = y;
+    }
+
+    // a method that checks if the user's input year is a leap year
+    public boolean isLeapYear()
+    {
+        // TO DO: complete
+    }
+
+    // a method that checks if the user's input year is a cicada year
+    public boolean isCicadaYear()
+    {
+        // TO DO: complete
+    }
+
+    // a method that check if the user's input year is a sunspot year
+    public boolean isSunspotYear()
+    {
+        // TO DO: complete
+    }
+}
