f959f16155d93aee7b87ca4faf13b532ea04dea3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2014-10-30 09:27:51 -0400

    Adding in a Lab8Main just to test input on a single line. Also, updated pre file.

---
+++ labs/lab8/Lab8Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Lab8Main {
+    
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int a, b, c, d;
+       a = scan.nextInt();
+       b = scan.nextInt();
+       c = scan.nextInt();
+       d = scan.nextInt();
+       System.out.println(a + b + c + d);
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+    }
+
+}
