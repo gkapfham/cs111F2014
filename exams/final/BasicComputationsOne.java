4f341d95b46ab46977684b7705b3d4e3ae2023ac
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2014-12-11 07:35:31 -0500

    Adding in all of the Programs.

---
+++ exams/final/BasicComputationsOne.java
@@ -0,0 +1,16 @@
+    // perform a series of arithmetic computations
+    public class BasicComputationsOne
+    {
+      public static void main(String[] args)
+      {
+        // declare and initialize the variables
+        double a = 4.0;
+        int b = 7;
+        int c = 5;
+        // perform the computation
+        a = a - b / 6 * c + b;
+        // create the output
+        System.out.println(" a = " + a);
+      }
+    }
+
---
+++ exams/final/BasicComputationsTwo.java
@@ -0,0 +1,18 @@
+    // perform more arithmetic computations
+    public class BasicComputationsTwo
+    {
+      public static void main(String[] args)
+      {
+        // declare and initialize the variables
+        int a, b, c, d, r;
+        a = 1;
+        b = 5;
+        c = 10;
+        d = 15;
+        // perform the computation
+        r = a - b * c / d + (int)Math.sqrt(a + d);
+        // produce the output
+        System.out.println(" The answer is: " + r);
+      }
+    }
+
